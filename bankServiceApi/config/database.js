98cebacd733cfaddfe921e0711bd62e7ba318c90
Emmanuel Tope-Ojo <topeojo@fisshboneandlestr.com>
2021-08-17 01:29:01 +0100

    Moved database info to an env file

--- bankServiceApi/config/database.js
+++ bankServiceApi/config/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', '134.209.30.188'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 10255),
-        database: env('DATABASE_NAME', 'test-strapi'),
-        username: env('DATABASE_USERNAME', 'celd'),
-        password: env('DATABASE_PASSWORD', 'KMbGZzliP47WAWHJXs27Id527KEiJWhwZUuK3mLU7lcKUWFK46HdMRpuObZoojOrZWeCpGtRcWL9mJnz2GN8tQ=='),
+        host: env("DATABASE_HOST"),
+        srv: env.bool("DATABASE_SRV"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', false),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE"),
+        ssl: env.bool("DATABASE_SSL"),
       },
     },
   },
---
+++ bankServiceApi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '37449735-3b8e-4e57-a78a-545a031c4b2c'
+};
